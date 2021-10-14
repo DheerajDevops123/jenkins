a21b8347e26481c611fa4f52153f65faa340bf7f
Dheeraj Devops <sai.dheeraj.52056@example.com>
2021-10-14 16:41:36 +0530

    jenkins push

---
+++ vars/roboshop.groovy
@@ -0,0 +1,45 @@
+def call() {
+  pipeline {
+    agent {
+      node {
+        label 'NODEJS'
+      }
+    }
+    stages {
+      stage('compile') {
+        steps {
+          echo ' Nothing to do with Compilation'
+        }
+      }
+      stage('Check the Code Quality') {
+        steps {
+          echo ' Code Quality'
+        }
+      }
+      stage('Lint Checks') {
+        steps {
+          echo 'Lint Checks'
+        }
+      }
+      stage('Unit Tests'){
+        steps {
+          echo 'Unit Tests'
+        }
+      }
+      stage('Prepare Artifacts') {
+        steps {
+          sh '''
+          cd static
+          zip -r frontend.zip *
+        '''
+        }
+      }
+      stage('Publish Artifacts') {
+        steps {
+          echo 'Publish Artifacts'
+        }
+      }
+    }
+  }
+}
+
