77bab1370378c469191b59eeaded22da9ea1c9ba
Dheeraj Devops <sai.dheeraj.52056@example.com>
2021-10-14 18:02:45 +0530

    jenkins push

--- vars/roboshop.groovy
+++ vars/roboshop.groovy
@@ -1,31 +1,38 @@
 def call(String AGENT, String COMPONENT) {
   pipeline {
+
     agent {
       node {
         label "${AGENT}"
       }
     }
+
     stages {
+
       stage('compile') {
         steps {
           echo ' Nothing to do with Compilation'
         }
       }
+
       stage('Check the Code Quality') {
         steps {
           echo ' Code Quality'
         }
       }
+
       stage('Lint Checks') {
         steps {
           echo 'Lint Checks'
         }
       }
+
       stage('Unit Tests') {
         steps {
           echo 'Unit Tests'
         }
       }
+
       stage('Prepare Artifacts') {
         steps {
           sh """
@@ -34,6 +41,7 @@ def call(String AGENT, String COMPONENT) {
         """
         }
       }
+
       stage('Publish Artifacts') {
         steps {
           echo 'Publish Artifacts'
