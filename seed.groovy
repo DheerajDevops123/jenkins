c06c6b528ac1cdca4e529a14423637bdfc493bad
Dheeraj Devops <sai.dheeraj.52056@example.com>
2021-10-17 19:17:00 +0530

    jenkins push

--- seed.groovy
+++ seed.groovy
@@ -25,26 +25,26 @@ folder('CI-Pipelines') {
 }
 
 def component = ["cart", "catalogue", "user", "frontend", "shipping", "payment"]
-
 def count= (component.size)
-
-pipelineJob('CI-Pipelines/frontend') {
-  configure { flowdefinition ->
-    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-        'userRemoteConfigs' {
-          'hudson.plugins.git.UserRemoteConfig' {
-            'url'('https://saidheeraj52056@dev.azure.com/saidheeraj52056/RoboShop/_git/frontend')
+for(int i=0;i<count;i++) {
+  pipelineJob('CI-Pipelines/${component[i]}') {
+    configure { flowdefinition ->
+      flowdefinition << delegate.'definition'(class: 'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition', plugin: 'workflow-cps') {
+        'scm'(class: 'hudson.plugins.git.GitSCM', plugin: 'git') {
+          'userRemoteConfigs' {
+            'hudson.plugins.git.UserRemoteConfig' {
+              'url'('https://saidheeraj52056@dev.azure.com/saidheeraj52056/RoboShop/_git/${component[i]}')
+            }
+          }
+          'branches' {
+            'hudson.plugins.git.BranchSpec' {
+              'name'('*/main')
+            }
           }
         }
-        'branches' {
-          'hudson.plugins.git.BranchSpec' {
-            'name'('*/main')
-          }
-        }
+        'scriptPath'('Jenkinsfile')
+        'lightweight'(true)
       }
-      'scriptPath'('Jenkinsfile')
-      'lightweight'(true)
     }
   }
 }
