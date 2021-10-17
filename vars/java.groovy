dbc87c045275fa1e226c1c699a281d529e9a4c2a
Dheeraj Devops <sai.dheeraj.52056@example.com>
2021-10-17 20:14:23 +0530

    jenkins push

---
+++ vars/java.groovy
@@ -0,0 +1,64 @@
+def call(String COMPONENT) {
+  pipeline {
+
+    agent {
+      node {
+        label "JAVA"
+      }
+    }
+
+    environment {
+      SONAR_KEY = credentials('SONAR_TOKEN')
+    }
+
+    stages {
+
+      stage('Code Compile') {
+        steps {
+          sh 'mvn compile'
+        }
+      }
+
+      stage('Check the Code Quality') {
+        steps {
+          sh """
+            sonar-scanner -Dsonar.projectKey=${COMPONENT} -Dsonar.sources=. -Dsonar.host.url=http://172.31.0.37:9000 -Dsonar.login=${SONAR_KEY}
+          """
+        }
+      }
+
+      stage('Lint Checks') {
+        steps {
+          echo 'Lint Checks'
+        }
+      }
+
+      stage('Unit Tests') {
+        steps {
+          echo 'Unit Tests'
+        }
+      }
+
+      stage('Prepare Artifacts') {
+        steps {
+          sh """
+          cd static
+          zip -r ${COMPONENT}.zip *
+        """
+        }
+      }
+
+      stage('Publish Artifacts') {
+        steps {
+          echo 'Publish Artifacts'
+        }
+      }
+    }
+
+    post {
+      always {
+        cleanWs()
+      }
+    }
+  }
+}
