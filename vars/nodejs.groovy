33aa248182d3e3fe053013256542dab15c953b69
Dheeraj Devops <sai.dheeraj.52056@example.com>
2021-10-18 11:01:04 +0530

    jenkins push

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -32,7 +32,7 @@ def call(String COMPONENT) {
       stage('Lint Checks') {
         steps {
 //          echo 'Lint Checks'
-            sh "/home/centos/node_modules/eslint/bin/eslint.js"
+            sh "/home/centos/node_modules/jslint/bin/jslint.js"
         }
       }
 
