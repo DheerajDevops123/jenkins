217e3280b2b936910d6659bd7240714693e966ea
Dheeraj Devops <sai.dheeraj.52056@example.com>
2021-10-21 09:50:33 +0530

    jenkins push

--- vars/nginx.groovy
+++ vars/nginx.groovy
@@ -58,7 +58,7 @@ def call(String COMPONENT) {
             cd static
             VERSION=`echo ${GIT_BRANCH}|awk -F / '{print \$NF}'`
             zip -r ${COMPONENT}-\${VERSION}.zip *
-        """
+         """
         }
       }
 
