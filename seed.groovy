97c5074c982d66fa0747f92192dab2c9b4185429
Dheeraj Devops <sai.dheeraj.52056@example.com>
2021-10-31 08:02:38 +0530

    jenkins push

--- seed.groovy
+++ seed.groovy
@@ -1,24 +1,3 @@
-//pipelineJob('CI-Pipelines/frontend') {
-//  configure { flowdefinition ->
-//    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-//      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-//        'userRemoteConfigs' {
-//          'hudson.plugins.git.UserRemoteConfig' {
-//            'url'('https://saidheeraj52056@dev.azure.com/saidheeraj52056/RoboShop/_git/frontend')
-//          }
-//        }
-//        'branches' {
-//          'hudson.plugins.git.BranchSpec' {
-//            'name'('*/main')
-//          }
-//        }
-//      }
-//      'scriptPath'('Jenkinsfile')
-//      'lightweight'(true)
-//    }
-//  }
-//}
-
 folder('CI-Pipelines') {
   displayName('CI-Pipelines')
   description('CI-Pipelines')
@@ -53,3 +32,72 @@ for(int i=0;i<count;i++) {
     }
   }
 }
+
+folder('Mutable') {
+  displayName('Mutable')
+  description('Mutable')
+}
+
+pipelineJob('Mutable/vpc') {
+  configure { flowdefinition ->
+    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+        'userRemoteConfigs' {
+          'hudson.plugins.git.UserRemoteConfig' {
+            'url'('https://saidheeraj52056@dev.azure.com/saidheeraj52056/RoboShop/_git/terraform-vpc')
+          }
+        }
+        'branches' {
+          'hudson.plugins.git.BranchSpec' {
+            'name'('*/main')
+          }
+        }
+      }
+      'scriptPath'('vpc/Jenkinsfile')
+      'lightweight'(true)
+    }
+  }
+}
+
+pipelineJob('Mutable/DB') {
+  configure { flowdefinition ->
+    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+        'userRemoteConfigs' {
+          'hudson.plugins.git.UserRemoteConfig' {
+            'url'('https://saidheeraj52056@dev.azure.com/saidheeraj52056/RoboShop/_git/terraform-vpc')
+          }
+        }
+        'branches' {
+          'hudson.plugins.git.BranchSpec' {
+            'name'('*/main')
+          }
+        }
+      }
+      'scriptPath'('databases/Jenkinsfile')
+      'lightweight'(true)
+    }
+  }
+}
+
+
+//pipelineJob('CI-Pipelines/frontend') {
+//  configure { flowdefinition ->
+//    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+//      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+//        'userRemoteConfigs' {
+//          'hudson.plugins.git.UserRemoteConfig' {
+//            'url'('https://saidheeraj52056@dev.azure.com/saidheeraj52056/RoboShop/_git/frontend')
+//          }
+//        }
+//        'branches' {
+//          'hudson.plugins.git.BranchSpec' {
+//            'name'('*/main')
+//          }
+//        }
+//      }
+//      'scriptPath'('Jenkinsfile')
+//      'lightweight'(true)
+//    }
+//  }
+//}
