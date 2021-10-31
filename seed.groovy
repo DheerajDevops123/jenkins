be830a95f52d692ef5ed84e6e01d16fe4a1dba7f
Dheeraj Devops <sai.dheeraj.52056@example.com>
2021-10-31 16:58:55 +0530

    jenkins push

--- seed.groovy
+++ seed.groovy
@@ -81,6 +81,35 @@ pipelineJob('Mutable/DB') {
 }
 
 
+for(int i=0;i<count;i++) {
+  def j = component[i]
+  pipelineJob("Mutable/${j}") {
+    configure { flowdefinition ->
+      flowdefinition << delegate.'definition'(class: 'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition', plugin: 'workflow-cps') {
+        'scm'(class: 'hudson.plugins.git.GitSCM', plugin: 'git') {
+          'userRemoteConfigs' {
+            'hudson.plugins.git.UserRemoteConfig' {
+              'url'("https://saidheeraj52056@dev.azure.com/saidheeraj52056/RoboShop/_git/${j}")
+              'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
+            }
+          }
+          'branches' {
+            'hudson.plugins.git.BranchSpec' {
+              'name'('**/tags/**')
+            }
+            'hudson.plugins.git.BranchSpec' {
+              'name'('*/main')
+            }
+          }
+        }
+        'scriptPath'('Jenkinsfile-infra')
+        'lightweight'(true)
+      }
+    }
+  }
+}
+
+
 //pipelineJob('CI-Pipelines/frontend') {
 //  configure { flowdefinition ->
 //    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
