8a74ce431ce199353d19d462ed59b5735e56b1ba
Dheeraj Devops <sai.dheeraj.52056@example.com>
2021-11-09 17:16:24 +0530

    jenkins push

--- vars/nginx.groovy
+++ vars/nginx.groovy
@@ -76,15 +76,16 @@ def call(String COMPONENT) {
         when { expression { sh([returnStdout: true, script: 'echo ${GIT_BRANCH} | grep tags || true']) } }
         steps {
           script {
-            def VERSION=GIT_BRANCH.split("/").last()
+            def VERSION = GIT_BRANCH.split("/").last()
             build job: 'AppDeploy', parameters: [string(name: 'COMPONENT', value: "${COMPONENT}"), string(name: 'ENV', value: 'dev'), string(name: 'APP_VERSION', value: "${VERSION}")]
           }
+        }
       }
-    }
 
-    post {
-      always {
-        cleanWs()
+      post {
+        always {
+          cleanWs()
+        }
       }
     }
   }
